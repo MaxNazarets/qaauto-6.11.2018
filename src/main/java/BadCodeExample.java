caeaa581c515564586128b6ac7bbbf38286802e5
Max Nazarets <max.nazarets@gmail.com>
2018-11-13 19:22:21 +0200

    changes for Lesson #2

--- src/main/java/BadCodeExample.java
+++ src/main/java/BadCodeExample.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class BadCodeExample {
     public static void main(String[] args) {
+
         WebDriver webDriver = new ChromeDriver();
         webDriver.get("https://google.com");
         WebElement searchField = webDriver.findElement(By.xpath("//input[@name='q']"));
@@ -15,14 +16,18 @@ public class BadCodeExample {
         List<WebElement> resultsList = webDriver.findElements(By.xpath("//div[@class='srg']/div[@class='g']"));
         System.out.println("Results on screen are :" + resultsList.size());
 
-//        for (int i = 0; i < resultsList.size(); i++) {
-//            if (resultsList.isEmpty()) {
-//                System.out.println("Here is Empty");
-//            }
-//            else {
-//                System.out.println(resultsList.get(i).getText());
-//            }
-//        }
+        String searchTerm = "Selenium";
+
+
+        for (int i = 0; i < resultsList.size(); i++) {
+            System.out.println(resultsList.get(i).getText());
+
+            if (resultsList.contains(searchTerm)) {
+                System.out.println("searchTerm found");
+            } else {
+                System.out.println("searchTerm is not found");
+            }
+        }
 
         webDriver.quit();
     }
