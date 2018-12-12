46967299ea39e9cd4477420686a7beec70cc2be3
Max Nazarets <max.nazarets@gmail.com>
2018-12-12 19:16:22 +0200

    little fixes

--- src/test/java/page/HomePage.java
+++ src/test/java/page/HomePage.java
@@ -7,8 +7,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import static java.lang.Thread.sleep;
-
 /**
  * PageObject class for HomePage.
  */
